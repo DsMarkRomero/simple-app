da76de370561c526671294b00ea00d9a38505e4e
DSMarkRomero <ds.geraldoromero@gmail.com>
2024-11-15 11:40:00 +0100

    Test MASTER 01

--- prime-number-frontend/src/PrimeCheck.js
+++ prime-number-frontend/src/PrimeCheck.js
@@ -27,7 +27,7 @@ const PrimeCheck = () => {
     return (
         <div>
             <h1>Prime Number Checker</h1>
-			<h2> Cambio para activar el trigger v69</h2>
+			<h2> Cambio para activar el trigger v1 en MASTER</h2>
             <button onClick={fetchVersion}>Get API Version</button>
             <p>API Version: {version}</p>
             <input
