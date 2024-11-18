632df26e4e9e54679ee236828b4c3489b879568e
DSMarkRomero <ds.geraldoromero@gmail.com>
2024-11-18 12:13:07 +0100

    test en master v2

--- prime-number-frontend/src/PrimeCheck.js
+++ prime-number-frontend/src/PrimeCheck.js
@@ -27,7 +27,7 @@ const PrimeCheck = () => {
     return (
         <div>
             <h1>Prime Number Checker</h1>
-			<h2> HOLA EN MASTER</h2>
+			<h2> HOLA EN MASTER v2</h2>
             <button onClick={fetchVersion}>Get API Version</button>
             <p>API Version: {version}</p>
             <input
