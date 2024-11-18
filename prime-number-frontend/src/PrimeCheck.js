4852458811ee77839954ba85a8a6cde2389d5173
DSMarkRomero <ds.geraldoromero@gmail.com>
2024-11-18 18:54:57 +0100

    test 3 de integracion completa en develop

--- prime-number-frontend/src/PrimeCheck.js
+++ prime-number-frontend/src/PrimeCheck.js
@@ -27,7 +27,7 @@ const PrimeCheck = () => {
     return (
         <div>
             <h1>Prime Number Checker</h1>
-			<h2> Integración completa DEVELOP v2</h2>
+			<h2> Integración completa DEVELOP v3</h2>
             <button onClick={fetchVersion}>Get API Version</button>
             <p>API Version: {version}</p>
             <input
