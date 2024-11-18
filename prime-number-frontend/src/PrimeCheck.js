97b171d5d84c4986f5eb9eb2dc4ebfe9430bfeef
DSMarkRomero <ds.geraldoromero@gmail.com>
2024-11-18 20:14:49 +0100

    test v6 master xd

--- prime-number-frontend/src/PrimeCheck.js
+++ prime-number-frontend/src/PrimeCheck.js
@@ -27,7 +27,7 @@ const PrimeCheck = () => {
     return (
         <div>
             <h1>Prime Number Checker</h1>
-			<h2> Implementación completa v6</h2>
+			<h2> RAMA MASTER v6</h2>
             <button onClick={fetchVersion}>Get API Version</button>
             <p>API Version: {version}</p>
             <input
